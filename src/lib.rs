28cc0f191f805fe2780050613f678f64998b4783
Ririsoft <riri@ririsoft.com>
2020-08-27 12:05:48 +0200

    return child directories

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,11 @@
 
 //! An utility for walking through a directory asynchronously and recursively.
 //!
+//! Based on [async-fs](https://docs.rs/async-fs) and [blocking](https://docs.rs/blocking),
+//! it uses a thread pool to handle blocking IOs. Please refere to those crates for the rationale.
+//! This crate is compatible with any async runtime based on [futures 0.3](https://docs.rs/futures-core),
+//! which includes [tokio](https://docs.rs/tokio), [async-std](https://docs.rs/async-std) and [smol](https://docs.rs/smol).
+//!
 //! # Example
 //!
 //! ```
@@ -22,7 +27,7 @@
 //! use futures_lite::stream::StreamExt;
 //!
 //! block_on(async {
-//!     let mut entries = WalkDir::new("my_directory").await?;
+//!     let mut entries = WalkDir::new("my_directory");
 //!     loop {
 //!         match entries.next().await {
 //!             Some(Ok(entry)) => println!("file: {}", entry.path().display()),
@@ -33,8 +38,7 @@
 //!             None => break,
 //!         }
 //!     }
-//! })?;
-//! # Ok::<(), std::io::Error>(())
+//! });
 //! ```
 
 #![forbid(unsafe_code)]
@@ -44,31 +48,35 @@ use std::path::{Path, PathBuf};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-use async_fs::read_dir;
+use async_fs::{read_dir, ReadDir};
 use futures_lite::future::{Boxed as BoxedFut, FutureExt};
 use futures_lite::stream::{self, Stream, StreamExt};
 
+#[doc(no_inline)]
 pub use async_fs::DirEntry;
-pub use futures_lite::stream::Boxed as BoxedStream;
+#[doc(no_inline)]
 pub use std::io::Result;
+
+type BoxStream = futures_lite::stream::Boxed<Result<DirEntry>>;
 
 /// A `Stream` of `DirEntry` generated from recursively traversing
 /// a directory.
 ///
-/// Entries are returned without a specific ordering.
+/// Entries are returned without a specific ordering. The top most root directory
+/// is not returned but child directories are.
 ///
 /// # Panics
 ///
 /// Panics if the directories depth overflows `usize`.
 pub struct WalkDir {
-    entries: BoxedStream<Result<DirEntry>>,
+    entries: BoxStream,
 }
 
 impl WalkDir {
     /// Returns a new `Walkdir` starting at `root`.
     pub fn new(root: impl AsRef<Path>) -> Self {
         Self {
-            entries: walk_dir_internal(root),
+            entries: walk_dir(root),
         }
     }
 }
@@ -82,12 +90,12 @@ impl Stream for WalkDir {
     }
 }
 
-fn walk_dir_internal(root: impl AsRef<Path>) -> BoxedStream<Result<DirEntry>> {
+fn walk_dir(root: impl AsRef<Path>) -> BoxStream {
     stream::unfold(State::Start(root.as_ref().to_owned()), |state| async move {
         match state {
             State::Start(root) => match read_dir(root).await {
                 Err(e) => return Some((Err(e), State::Done)),
-                Ok(rd) => return walk(vec![rd.boxed()]).await,
+                Ok(rd) => return walk(vec![rd]).await,
             },
             State::Walk(dirs) => return walk(dirs).await,
             State::Done => return None,
@@ -98,35 +106,43 @@ fn walk_dir_internal(root: impl AsRef<Path>) -> BoxedStream<Result<DirEntry>> {
 
 enum State {
     Start(PathBuf),
-    Walk(Vec<BoxedStream<Result<DirEntry>>>),
+    Walk(Vec<ReadDir>),
     Done,
 }
 
-fn walk(
-    mut dirs: Vec<BoxedStream<Result<DirEntry>>>,
-) -> BoxedFut<Option<(Result<DirEntry>, State)>> {
+type UnfoldState = Option<(Result<DirEntry>, State)>;
+
+fn walk(mut dirs: Vec<ReadDir>) -> BoxedFut<UnfoldState> {
     async move {
         if let Some(dir) = dirs.last_mut() {
             match dir.next().await {
-                Some(Ok(entry)) => match entry.file_type().await {
-                    Err(e) => return Some((Err(e), State::Walk(dirs))),
-                    Ok(ft) if ft.is_dir() => {
-                        let wd = walk_dir_internal(entry.path());
-                        dirs.push(wd);
-                        return walk(dirs).await;
-                    }
-                    Ok(_) => return Some((Ok(entry), State::Walk(dirs))),
-                },
-                Some(Err(e)) => return Some((Err(e), State::Walk(dirs))),
+                Some(Ok(entry)) => walk_entry(entry, dirs).await,
+                Some(Err(e)) => Some((Err(e), State::Walk(dirs))),
                 None => {
                     dirs.pop();
-                    return walk(dirs).await;
+                    walk(dirs).await
                 }
             }
+        } else {
+            None
         }
-        None
     }
     .boxed()
+}
+
+async fn walk_entry(entry: DirEntry, mut dirs: Vec<ReadDir>) -> UnfoldState {
+    match entry.file_type().await {
+        Err(e) => Some((Err(e), State::Walk(dirs))),
+        Ok(ft) if ft.is_dir() => {
+            let rd = match read_dir(entry.path()).await {
+                Err(e) => return Some((Err(e), State::Done)),
+                Ok(rd) => rd,
+            };
+            dirs.push(rd);
+            Some((Ok(entry), State::Walk(dirs)))
+        }
+        Ok(_) => Some((Ok(entry), State::Walk(dirs))),
+    }
 }
 
 #[cfg(test)]
@@ -172,23 +188,23 @@ mod tests {
             async_fs::write(&f2, []).await?;
             async_fs::write(&f3, []).await?;
 
-            let mut got = Vec::new();
-            let want = vec![f3.to_owned(), f2.to_owned(), f1.to_owned()];
+            let want = vec![
+                d1.to_owned(),
+                d2.to_owned(),
+                f3.to_owned(),
+                f2.to_owned(),
+                f1.to_owned(),
+            ];
             let mut wd = WalkDir::new(root.path());
 
-            let next = wd.next().await.unwrap()?;
-            got.push(next.path());
-
-            let next = wd.next().await.unwrap()?;
-            got.push(next.path());
-
-            let next = wd.next().await.unwrap()?;
-            got.push(next.path());
-
+            let mut got = Vec::new();
+            while let Some(entry) = wd.next().await {
+                let entry = entry.unwrap();
+                got.push(entry.path());
+            }
             got.sort();
             assert_eq!(got, want);
 
-            assert!(wd.next().await.is_none());
             Ok(())
         })
     }
