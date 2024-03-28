ba2f74919274a31b5c209e7fca4551d32b35ec39
Jordan Danford <jordandanford@gmail.com>
2024-03-28 08:54:38 -0700

    Bump major version, add copyright header, format code

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 use std::{
-    error, fmt, io, ops::Deref, path::{Path, PathBuf}
+    error, fmt, io,
+    ops::Deref,
+    path::{Path, PathBuf},
 };
 
 /// A wrapper around [`io::Error`] that includes the associated path.
