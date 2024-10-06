4564802ea7a2e2fe63c35f1136ca36862deb6c59
tharvik <tharvik@users.noreply.github.com>
2024-10-06 22:22:19 +0200

    error: add io::Error conversion

--- src/error.rs
+++ src/error.rs
@@ -27,6 +27,26 @@ impl Error {
         let InnerError::Io { ref source, .. } = self.0;
         Some(source)
     }
+
+    /// Similar to [`io`][Self::io] except consumes self to convert to the original
+    /// [`io::Error`] if one exists.
+    pub fn into_io(self) -> Option<io::Error> {
+        let InnerError::Io { source, .. } = self.0;
+        Some(source)
+    }
+}
+
+impl From<Error> for io::Error {
+    /// Convert to an [`io::Error`], preserving the original [`struct@Error`]
+    /// as the ["inner error"][io::Error::into_inner].
+    /// Note that this also makes the display of the error include the context.
+    ///
+    /// This is different from [`into_io`][Error::into_io] which returns
+    /// the original [`io::Error`].
+    fn from(err: Error) -> io::Error {
+        let InnerError::Io { ref source, .. } = err.0;
+        io::Error::new(source.kind(), err)
+    }
 }
 
 #[derive(Debug, Error)]
